33532c736b8d678a3ec72b5c0ad64b4c6323ea62
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-07-30 08:51:02 -0300

    Mf valor errado
    - Calculando corretamente o mf
    - Ajustando o controlador para a nova função de transferência

--- main.m
+++ main.m
@@ -1,7 +1,8 @@
 clc; clear;
-% Entradas
+%% Entradas
 
-Aa = 1.7453e-4; %Absorver cross-section area
+%Dados Constantes do problema
+Aa = 1.7453e-4;
 Cp = 4190;
 H = 4;
 Leq = 5.67;
@@ -11,52 +12,46 @@ dc = 0;
 neq = 588;
 Ta = 25;
 
-% Ponto de Operação
-mf_po = 2.55;
-Tbarra = 15;
-Tif = 70;
-Tof = Tif + Tbarra;
-
-%Temperature
-% T = (T0f + Tif) / 2;
-
-% Equivalent Mass Flow
-% meq = mf/neq
-
-nl = 4;
-nc = 7;
-ncp = 3;
-na = 7;
+nl=4; nc=7; ncp=3; na=7;
 neq = nl*nc*ncp*na;
+
+%p*Cp*Aa*Tof = Bl*I - H(T - Ta)/Leq - Cp*meq*(Tof-Tif)/Leq;
+%Entradas e saídas no equilíbrio
+Tbarra = 15;
+Tif = 70; %u2
+Tof = Tif + Tbarra; %y
+I = 900; %u3
+Tof_deriv = 0; %y'
+meq = -( p*Cp*Aa*Tof_deriv*Leq -Bl*I*Leq +H*(Tof-Tif-Ta) ) / ( Cp*(Tof-Tif) );
+mf = meq * neq; %u1
 
 %Tof= saída do sistema
 % I = irradiação (perturbação)
 % Tif = temperatura de entrada (perturbação)
-% meq = entrada do sistema em malha aberta
-I = 900;
+% mf = entrada do sistema em malha aberta
 
-%ESPACO DE ESTADOS LINEARIZADO
+%% LINEARIZAÇÃO DO SISTEMA
 
-A = -( (H/(2*Leq)) + ((Cp*mf_po)/(neq*Leq)) ) / (p*Cp*Aa);
+%Espaço de estados
+A = -( (H/(2*Leq)) + ((Cp*mf)/(neq*Leq)) ) / (p*Cp*Aa);
 B = -( Cp*Tof ) / ( neq*Leq*p*Cp*Aa );
 C = [1];
 D = [0];
 
+%Função de transferência
 [NUM,DEN] = ss2tf(A,B,C,D);
-
-%Planta
 G = tf(NUM,DEN);
 
-%Pós PID TUNER
-kp = - 0.0797;
-ki = - 0.0030554	;
+%% CONTROLE DO SISTEMA
+
+%Controlador PI
+
+% Settling time = 21.7s
+% Overshoot = 6%
+kp = - 5.045;
+ki = - 0.3851;
 io = 0;
-
-%Controlador
 C = pid (kp,ki);
-
-%Função de transferência completa
-T = feedback(C*G,1);
 
 %Função de transferência das pertubações
 P1 = G;
@@ -65,4 +60,3 @@ P2 = G;
 %Controlador feedfoward
 Gff1 = -P1/G;
 Gff2 = -P2/G;
-
